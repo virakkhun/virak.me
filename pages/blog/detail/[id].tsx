6308823174e7639e6555fca80c2c6be44365a4b9
virakkhun <rakmanoban@gmail.com>
2022-11-15 16:03:42 +0700

    chore: add styling on code block

--- pages/blog/detail/[id].tsx
+++ pages/blog/detail/[id].tsx
@@ -1,9 +1,22 @@
 import Head from 'next/head'
 import BlogLayout from '../../../components/layouts/blog-layout'
+
 import ReactMarkdown from 'react-markdown'
+import { Components } from 'react-markdown'
 import remark from 'remark-gfm'
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx'
+import typscript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript'
+import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash'
+import markdown from 'react-syntax-highlighter/dist/cjs/languages/prism/markdown'
+import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json'
+import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
+import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
 import rehypeRaw from 'rehype-raw'
-import { ReactElement } from 'react'
+import rangeParser from 'parse-numeric-range'
+import { nord } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+import React, { ReactElement, useEffect } from 'react'
 import { GetServerSideProps } from 'next'
 import { fetchApi } from '../../../utils/FetchApi'
 import { BlogDetail } from '../../../utils/types/blogs.type'
@@ -21,7 +34,87 @@ export const getServerSideProps: GetServerSideProps = async (event) => {
 	}
 }
 
+const languages: { name: string; obj: any }[] = [
+	{
+		name: 'tsx',
+		obj: tsx,
+	},
+	{
+		name: 'jsx',
+		obj: jsx,
+	},
+	{
+		name: 'bash',
+		obj: bash,
+	},
+	{
+		name: 'markdown',
+		obj: markdown,
+	},
+	{
+		name: 'typescript',
+		obj: typscript,
+	},
+	{
+		name: 'json',
+		obj: json,
+	},
+	{
+		name: 'javascript',
+		obj: javascript,
+	},
+]
+
+languages.forEach((l) => {
+	SyntaxHighlighter.registerLanguage(l.name, l.obj)
+})
+
 const BlogDetail: NextPageWithLayout<{ data: BlogDetail }> = ({ data }) => {
+	const syntaxTheme = nord
+
+	const MarkdownComponents: Components = {
+		code({ node, inline, className, ...props }) {
+			const match = /language-(\w+)/.exec(className || '')
+			const hasMeta = node.data?.meta as string
+			const meta = node.data?.meta as string
+
+			const applyHighlights: object = (applyHighlights: number) => {
+				if (hasMeta) {
+					const RE = /{([\d,-]+)}/
+					const metadata = meta.replace(/\s/g, '')
+					const strlineNumbers = RE?.test(metadata)
+						? RE.exec(metadata)![1]
+						: '0'
+
+					const highlightLines = rangeParser(strlineNumbers)
+					const highlight = highlightLines
+					const data: string | null = highlight.includes(applyHighlights)
+						? 'highlight'
+						: null
+
+					return { data }
+				} else {
+					return {}
+				}
+			}
+
+			return match ? (
+				<SyntaxHighlighter
+					style={syntaxTheme}
+					language={match[1]}
+					PreTag='div'
+					className='codeStyle'
+					wrapLines={hasMeta ? true : false}
+					useInlineStyles={true}
+					lineProps={applyHighlights}
+					{...props}
+				/>
+			) : (
+				<code className={className} {...props} />
+			)
+		},
+	}
+
 	return (
 		<>
 			<Head>
@@ -80,8 +173,12 @@ const BlogDetail: NextPageWithLayout<{ data: BlogDetail }> = ({ data }) => {
 						</div>
 					</div>
 				</div>
-				<div className='mt-6 bg-slate-900/50 p-4 rounded-md'>
-					<ReactMarkdown remarkPlugins={[remark]} rehypePlugins={[rehypeRaw]}>
+				<div className='mt-6 bg-slate-900 p-4 rounded-md'>
+					<ReactMarkdown
+						remarkPlugins={[remark]}
+						rehypePlugins={[rehypeRaw]}
+						components={MarkdownComponents}
+					>
 						{data.attributes.blog_detail.data.attributes.detail}
 					</ReactMarkdown>
 				</div>
