a969f5942d3a640058dfabf3509cba8cc7df2219
virakkhun <rakmanoban@gmail.com>
2023-06-25 21:46:09 +0700

    fixed import wrong module

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next'
 import Image from 'next/image'
-import VirakPhoto from '../assets/images/virak_khun.JPG'
+import VirakPhoto from '../assets/images/virak_khun.jpg'
 
 export async function generateMetadata(): Promise<Metadata> {
 	return {
