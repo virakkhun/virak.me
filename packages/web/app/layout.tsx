546021c5efd4f6e3723b048c3a7b30ff63bfd7fc
virakkhun <rakmanoban@gmail.com>
2023-07-01 22:04:13 +0700

    update ui

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Divider } from '@virak.me/ui'
-import { FooterComponent } from '../modules/core/components/footer.component'
-import { HeaderComponent } from '../modules/core/components/header.component'
+import { FooterComponent } from '../core/components/footer.component'
+import { HeaderComponent } from '../core/components/header.component'
 import '../assets/styles/globals.css'
 import '../assets/styles/util.css'
 
@@ -11,11 +11,13 @@ interface Props {
 export default function RootLayout({ children }: Props) {
 	return (
 		<html lang='en'>
-			<body className='bg-primary container'>
-				<HeaderComponent />
-				{children}
-				<Divider />
-				<FooterComponent />
+			<body className='bg-dark-gray-0'>
+				<div className='md:px-32 px-4'>
+					<HeaderComponent />
+					{children}
+					<Divider />
+					<FooterComponent />
+				</div>
 			</body>
 		</html>
 	)
