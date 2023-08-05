dc284a5ffe9aa0a0c0ffd1d484c489c644a6d9da
virakkhun <rakmanoban@gmail.com>
2023-08-05 22:40:58 +0700

    chore: fixed style broken

--- src/components/powered.component.svelte
+++ src/components/powered.component.svelte
@@ -5,5 +5,10 @@
 <div class="flex justify-center items-center w-full absolute bottom-4 left-0">
 	<p>
 		{@html $t('frameworkRef')}
+		<a
+			href="https://kit.svelte.dev"
+			target="_blank"
+			class="text-orange-600 font-bold hover:text-orange-700 duration-200">SvelteKit</a
+		>
 	</p>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,9 @@
 <div class="w-full h-screen flex justify-center items-center relative">
 	<div class="flex flex-col gap-4 items-center md:w-1/2 w-full md:px-0 px-4 text-lg text-center">
 		<p>{$t('author.greet')}</p>
-		<p>{@html $t('author.name')}</p>
+		<p>
+			{@html $t('author.name')}
+		</p>
 		<p>
 			{@html $t('author.job')}
 		</p>
