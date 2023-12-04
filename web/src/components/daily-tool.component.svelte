d8f7e1835ebb1c07f4b790ca09fd295db30fdb7b
Virak Khun <virak.khun@okoone.com>
2023-12-04 22:45:37 +0700

    add daily tools

---
+++ web/src/components/daily-tool.component.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { DAILY_TOOLS_ICON_LINK } from '../shared/constants/daily-tool-icon-link';
+	import { t } from '../shared/services/i18n.service';
+</script>
+
+<p class="text-center text-2xl mt-40">{$t('dailyTool')}</p>
+<div class="flex items-center gap-2">
+	{#each DAILY_TOOLS_ICON_LINK as tool}
+		<a
+			href={tool.refLink}
+			target="_blank"
+			class="border border-white/5 backdrop-blur-md w-32 h-32 rounded-md flex justify-center items-center hover:drop-shadow-emerald transition-all duration-300"
+		>
+			<img src={tool.iconLink} alt="Virak's daily tool" width="64" height="64" loading="lazy" />
+		</a>
+	{/each}
+</div>
