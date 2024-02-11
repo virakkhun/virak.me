13a7f1016ffd61f38f77513fc551fccb1080babc
Virak Khun <virak.khun@okoone.com>
2024-02-11 20:21:29 +0700

    chore: remove unused code

--- src/routes/say-hi/component/hi-title.component.svelte
+++ src/routes/say-hi/component/hi-title.component.svelte
@@ -1,7 +1,1 @@
-<script lang="ts">
-	import { t } from '../../../shared/services/i18n.service';
-</script>
-
-<p class="font-bold text-accent md:text-4xl text-2xl mt-20">
-	{$t('hi.title')}
-</p>
+<p class="font-bold text-accent md:text-4xl text-2xl mt-24">Hi, glad to have you here...</p>
