385eb4929733f03ea6a0ee931232718f811abd9a
virakkhun <virak.dev.2000@gmail.com>
2024-08-06 21:51:14 +0700

    chore: add file-explorer ui to blog list

--- src/lib/blogs.ts
+++ src/lib/blogs.ts
@@ -1,5 +1,51 @@
-export default [
-	'basic-commands-in-linux.md',
-	'my-2023-stack.md',
-	'useful-method-of-array-in-javascript.md'
+import DirOpen from '../assets/icons/dir-open.svg';
+import Rss from '../assets/icons/rss.svg';
+
+export enum FsType {
+	DIR = 'dir',
+	FILE = 'file'
+}
+
+export interface File {
+	type: FsType;
+	label: string;
+	link: string;
+	dirs?: Dir[];
+}
+
+export interface Dir {
+	type: FsType;
+	label: string;
+	files: File[];
+}
+
+export const FsIcons: Record<FsType, string> = {
+	[FsType.DIR]: DirOpen,
+	[FsType.FILE]: Rss
+};
+
+const blogs: Dir[] = [
+	{
+		type: FsType.DIR,
+		label: 'blogs explorer...',
+		files: [
+			{
+				type: FsType.FILE,
+				label: 'basic-commands-in-linux.md',
+				link: '/blogs/basic-commands-in-linux'
+			},
+			{
+				type: FsType.FILE,
+				label: 'my-2023-stack.md',
+				link: '/blogs/my-2023-stack'
+			},
+			{
+				type: FsType.FILE,
+				label: 'useful-method-of-array-in-javascript.md',
+				link: '/blogs/useful-method-of-array-in-javascript'
+			}
+		]
+	}
 ];
+
+export default blogs;
--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-	import blogs from '$lib/blogs';
+	import blogs, { FsType, FsIcons } from '$lib/blogs';
 	import Breadcrumb from '../../components/breadcrumb.svelte';
-	import BlogTitle from './components/blog-title.component.svelte';
 
 	const _breadcrumbs = [
 		{ link: '/', label: 'home' },
 		{ link: '/blogs', label: 'blogs' }
 	];
-	const filename = (v: string) => v.replace('.md', '');
-	const _blogs = blogs.map(filename);
 </script>
 
 <svelte:head>
@@ -17,11 +14,26 @@
 
 <Breadcrumb breadcrumbs={_breadcrumbs} />
 <div class="mt-24 pb-4">
-	<BlogTitle />
-
 	<div class="mt-4 flex flex-col gap-2 font-semibold">
-		{#each _blogs as b}
-			<a href="/blogs/{b}">{b}</a>
+		{#each blogs as b}
+			{#if b.type === FsType.DIR}
+				<div class="flex flex-col gap-2">
+					<div class="flex gap-1 items-center">
+						<img class="w-3 h-3" src={FsIcons[b.type]} alt="dir open icon" />
+						<p class="font-semibold">{b.label}</p>
+					</div>
+					<div class="flex flex-col gap-1 ml-4">
+						{#each b.files as file}
+							<div class="flex gap-1.5 items-center">
+								<img class="w-3 h-3" src={FsIcons[file.type]} alt="dir open icon" />
+								<a href={file.link} class="font-thin hover:text-text-secondary">
+									{file.label}
+								</a>
+							</div>
+						{/each}
+					</div>
+				</div>
+			{/if}
 		{/each}
 	</div>
 </div>
