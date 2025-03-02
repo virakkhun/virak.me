7bd72c442e8a463ad9d499659cf842b83c22c0c4
virakkhun <virak.dev.2000@gmail.com>
2025-03-02 22:17:28 +0700

    chore: ignore http-error

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,8 @@ const config = {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
 		prerender: {
-			handleMissingId: 'ignore'
+			handleMissingId: 'ignore',
+			handleHttpError: 'ignore'
 		}
 	}
 };
