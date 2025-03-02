59fda0072c8ceaec1d48367dc7d4d082d6fbc803
virakkhun <virak.dev.2000@gmail.com>
2025-03-02 21:23:28 +0700

    chore: test remove base-path

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: ''
 		},
 		prerender: {
 			handleMissingId: 'ignore'
