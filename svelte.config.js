02146086fc4e816d3a63c9a30be3430164b770bb
virakkhun <virak.dev.2000@gmail.com>
2025-03-02 20:19:43 +0700

    chore: migrate to sveltekit@2

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
-import vercelAdapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import staticAdapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: vercelAdapter({ runtime: 'nodejs20.x', isr: true })
+		adapter: staticAdapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
