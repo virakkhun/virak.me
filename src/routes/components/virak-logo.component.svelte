d2c6299309be500ca39ba88e4c9ea3fb8819d7b1
virakkhun <virak.dev.2000@gmail.com>
2025-03-02 22:30:19 +0700

    chore: add base to logo.png

--- src/routes/components/virak-logo.component.svelte
+++ src/routes/components/virak-logo.component.svelte
@@ -3,5 +3,11 @@
 </script>
 
 <a href="{base}/">
-	<img src="/favicon.png" alt="virak's logo" width="32" height="32" class="w-8 h-8 aspect-square" />
+	<img
+		src="{base}/favicon.png"
+		alt="virak's logo"
+		width="32"
+		height="32"
+		class="w-8 h-8 aspect-square"
+	/>
 </a>
