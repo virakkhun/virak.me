3cfefba5ce3f6a069b247ecaf233d4655afe7447
virakkhun <virak.dev.2000@gmail.com>
2025-03-02 22:08:58 +0700

    chore: fixed failed

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '/virak.me' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
 		prerender: {
 			handleMissingId: 'ignore'
